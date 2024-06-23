a4fdc0e9f1870f33f03caef1aff420fa783c2b60
scr6w <playerzone06@gmail.com>
2024-06-23 00:50:05 -0300

    chore: Add environment variables and API path to Flask server

--- server.py
+++ server.py
@@ -1,4 +1,13 @@
 from flask import Flask, request, jsonify, render_template
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+SERVER_PORT = os.getenv('SERVER_PORT', 8000)
+API_VERSION = os.getenv('API_VERSION', 'development')
+API_PATH = f'/api/{API_VERSION}/{API_VERSION}'
+
 
 app = Flask(__name__)
 
@@ -6,8 +15,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',{'api': API_PATH})
 
+@app.route(f'{API_PATH}/books/random', methods=['POST'])
+def process_data():
+    data = request.get_json()
+    response = {'message': 'ok'}
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
